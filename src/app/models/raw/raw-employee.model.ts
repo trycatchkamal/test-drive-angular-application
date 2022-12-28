5e218a92652b181c798a15cab8659d5724506189
Kamal Raj Sekar <notify.kamalraj@gmail.com>
2022-12-28 14:09:24 +0000

    08_passing_service_test_with_mock_repository

--- src/app/models/raw/raw-employee.model.ts
+++ src/app/models/raw/raw-employee.model.ts
@@ -1,2 +1,9 @@
 export interface RawEmployee {
+  number:string;
+  name: string;
+  bank_name: string;
+  location:string;
+  email:string;
+  mobile:string;
+  active:boolean;
  }
--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Employee } from '../models/employee.model';
 import { EmployeesRepository } from '../repositories/employees.repository';
+import { RawEmployee } from '../models/raw/raw-employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
 
-  constructor(private repository:EmployeesRepository) {
-
-  }
+  constructor(private repository:EmployeesRepository) {}
 
   getActiveEmployees():Observable<Employee[]>{
-    return of();
+    return this.repository.getRawEmployees().pipe(
+      map(this.toListOfActiveEmployees)
+    );
+  }
+  private toListOfActiveEmployees = (employees: RawEmployee[]): Employee[] => {
+    let result:Employee[]=[];
+
+    employees.forEach(item=> {
+      if(item.active ===true){
+        result.push(
+        {
+          displayName: item.name,
+          contact:item.mobile,
+          email: item.email,
+          location: item.location,
+          id: item.number,
+          bankName: item.bank_name,
+        });
+      }
+    })
+
+    return result;
   }
 }
