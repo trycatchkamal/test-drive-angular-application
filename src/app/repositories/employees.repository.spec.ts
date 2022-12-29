72bef76e7706e533e73e4140757be24568e2148b
Kamal Raj Sekar <notify.kamalraj@gmail.com>
2022-12-29 08:03:57 +0000

    10_failing_repository_test

--- src/app/repositories/employees.repository.spec.ts
+++ src/app/repositories/employees.repository.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { EmployeesRepository } from './employees.repository';
+import { Observable, of, from, pipe } from 'rxjs';
+import { RawEmployee } from '../models/raw/raw-employee.model';
 
 describe('EmployeesRepository', () => {
   let repository: EmployeesRepository;
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
     repository = TestBed.inject(EmployeesRepository);
@@ -11,5 +12,15 @@ describe('EmployeesRepository', () => {
 
   it('Should exist', () => {
     expect(repository).toBeTruthy();
+  });
+
+  it('should call underlying store for the data', (done: DoneFn) => {
+    const response = new Response(JSON.stringify({}));
+    spyOn(window,'fetch').and.resolveTo(response);
+
+    repository.getRawEmployees().subscribe(()=>{
+      expect(window.fetch).toHaveBeenCalled();
+      done();
+    })
   });
 });
