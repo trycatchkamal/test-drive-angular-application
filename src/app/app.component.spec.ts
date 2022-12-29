5db95677d14e88ca57f54b0a56bcb5a5c4cabf95
Kamal Raj Sekar <notify.kamalraj@gmail.com>
2022-12-29 07:46:23 +0000

    05_failing_root_component_test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -24,5 +25,13 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('test-drive-angular-application');
+  });
+
+
+  it('Should contain employee component',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    console.log(fixture.debugElement);
+    expect(fixture.debugElement.query(By.css('app-employees'))).not.toBeNull();
   });
 });
